df07450ad0c2cd4166b3dc631b35d5ace936e45b
Rigzin Tamang <tamangr@cc.corp>
2024-11-05 09:37:46 +1000

    remove node modules from tracking and add to .gitignore

---
+++ myApp.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+    res.send('This is POC for the RAPID Web Application');
+});
+
+app.listen(port, () => {
+    console.log(`Visit http://localhost:${port} to view the Application.`);
+});
