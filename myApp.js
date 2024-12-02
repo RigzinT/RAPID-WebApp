46dd1ff6660360bba6b45ca23dd61efa6ed8c973
RigzinT <rigzint@gmail.com>
2024-12-03 00:08:35 +1000

    test passed - deployment in server test

--- myApp.js
+++ myApp.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
             <title>RAPID web app is here!!!</title>
         </head>
         <body>
-            <h1>Arvo app test</h1>
+            <h1>Arvo app test with automation</h1>
             <form id="additionForm" action="/add" method="GET">
                 <label for="num1">Number 1 (can be a decimal):</label>
                 <input type="number" step="any" id="num1" name="num1" required>
