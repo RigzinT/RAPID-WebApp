3d2e4987351b60ff056d939c5b0d58fc4d26f283
RigzinT <rigzint@gmail.com>
2024-12-02 22:35:36 +1000

    test update

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ import express from 'express';
 const app = express();
 const port = 4000;
 
-// Serve static HTML for the form
+// Serve static HTML for the app
 app.get('/', (req, res) => {
     res.send(`
         <!DOCTYPE html>
